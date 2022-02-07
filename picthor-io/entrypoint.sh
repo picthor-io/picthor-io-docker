bd61be1d78e3fffe0bceb2dd2da0f0b2d0cc7c70
Vitali Carbivnicii <realcnbs@gmail.com>
2022-02-06 20:20:57 -0500

    Initial commit

---
+++ picthor-io/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+: "${PICTHOR_CACHE_DIR?PICTHOR_CACHE_DIR env var is required}"
+: "${PICTHOR_UI_STATIC_FILES_DIR?PICTHOR_UI_STATIC_FILES_DIR env var is required}"
+: "${PICTHOR_SITE_URL?PICTHOR_SITE_URL env var is required}"
+: "${PICTHOR_API_HOST?PICTHOR_API_HOST env var is required}"
+mkdir $PICTHOR_CACHE_DIR
+echo "{\"siteUrl\":\"$PICTHOR_SITE_URL\",\"apiHost\": \"$PICTHOR_API_HOST\"}" > $PICTHOR_UI_STATIC_FILES_DIR/env.json
+java -jar /root/picthor.io.jar
